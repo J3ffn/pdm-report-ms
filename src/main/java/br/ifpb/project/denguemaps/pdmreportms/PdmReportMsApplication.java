74090c455df671872e4667e6a4eac1da245fe50c
j3ffn <jefferson.mangueira12@gmail.com>
2025-10-09 17:43:45 -0300

    init project

---
+++ src/main/java/br/ifpb/project/denguemaps/pdmreportms/PdmReportMsApplication.java
@@ -0,0 +1,13 @@
+package br.ifpb.project.denguemaps.pdmreportms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmReportMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdmReportMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifpb/project/denguemaps/pdmreportms/PdmReportMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpb.project.denguemaps.pdmreportms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmReportMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
